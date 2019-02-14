4d7546a0f1fbd1c1f6daf661fd76cde70115939e
Joe Paris <joe.paris@outlook.com>
2019-02-14 13:49:43 -0800

    demos for Feb 12

---
+++ demos/19.02.12/index.js
@@ -0,0 +1,16 @@
+function foo() {
+  // for (const index = 0; index < 3; index++) {
+  //   alert(index);
+
+
+  // }
+  const myarr = [1, 2, 3];
+  alert(myarr);
+  myarr[0] = 42;
+  alert(myarr);
+  myarr = "a new thing";
+  alert(myarr);
+}
+
+// foo();
+// alert("The value of message is " + message);
